5f76b289863c63bd342556461b8c7a2cd07b5b79
Anders Fischer-Nielsen <andersfischern@me.com>
2016-03-15 14:54:09 +0100

    Added silly javascript function.

--- site.js
+++ site.js
@@ -1,3 +1,11 @@
-var heading = document.getElementsByTagName("h1")[0];
-debugger;
-heading.innerText = heading.innerText += " World!";
+var i = 0;
+
+window.setInterval(function() {
+		var text = "Hello World!";
+
+		var heading = document.getElementsByTagName("h1")[0];
+		heading.innerText = text.substring(0, i);
+		i++;
+
+		if (i == text.length+1) i = 0;
+}, 500);
